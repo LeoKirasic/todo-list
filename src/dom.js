5c6df5117aa49a283ac3106bae50321ab18325bb
Leo Kirasic <kirasicleo@gmail.com>
2021-09-02 23:36:11 +0200

    add modal popup form

---
+++ src/dom.js
@@ -0,0 +1,44 @@
+
+const renderDom = () => {
+    const content = document.querySelector('#content');
+    const createProjectButton = document.createElement('button');
+    content.appendChild(createProjectButton);
+    createProjectButton.textContent = 'Add project'
+        const modal = document.createElement('div');
+        modal.id = 'modal';
+        const modalContent = document.createElement('div');
+        modalContent.id = 'modal-content';
+        const header = document.createElement('div');
+        header.id = 'header';
+        const title = document.createElement('div');
+        title.textContent = 'Add project';
+        const closeButton = document.createElement('span');
+        closeButton.innerHTML = '&times';
+        closeButton.id = 'close-button';
+        const input = document.createElement('input');
+        const addButton = document.createElement('button');
+        addButton.id = 'add-button';
+        addButton.textContent = "Add"
+        header.appendChild(title);
+        header.appendChild(closeButton);
+        modalContent.appendChild(header);
+        modalContent.appendChild(input);
+        modalContent.appendChild(addButton);
+        modal.appendChild(modalContent);
+        content.appendChild(modal);
+
+        createProjectButton.addEventListener('click', () => {
+            modal.style.display = 'block';
+        });
+        closeButton.addEventListener('click', () =>{
+            modal.style.display = 'none';
+        });
+        window.addEventListener('click', () => {
+            if(event.target === modal) {
+            modal.style.display = 'none';
+                
+            }
+        });
+        }
+
+export{renderDom};
