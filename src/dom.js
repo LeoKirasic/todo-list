7babbb60da085220667b2be4fd21f5b976c97700
LeoKirasic <kirasicleo@gmail.com>
2021-09-21 19:54:00 +0200

    Add better todo display on submit button click

--- src/dom.js
+++ src/dom.js
@@ -16,7 +16,7 @@ function loadDom() {
     projectForm.appendChild(projectSubmitButton);
     projectHeader.appendChild(projectForm);
     content.appendChild(projectHeader);
-    const task = document.createElement('div');
+    const taskList = document.createElement('div');
 
     const projectList = document.createElement('div');
     projectList.id = 'project-list';
@@ -37,8 +37,9 @@ function loadDom() {
                 const index = projects.findIndex(item => item.title === e.target.textContent);
                 console.log(index);
                 console.log(projects[index].arrayOfTodos);
-                task.textContent = JSON.stringify(projects[index].arrayOfTodos);
-                tasks.appendChild(task);
+                // taskList.textContent = JSON.stringify(projects[index].arrayOfTodos);
+                taskList.appendChild(task);
+                tasks.appendChild(taskList);
             })
         });
     });
@@ -52,6 +53,7 @@ function loadDom() {
     const todoTitleInput = document.createElement('input');
     todoTitleInput.placeholder = 'Title';
     const todoDueDateInput = document.createElement('input');
+    todoDueDateInput.setAttribute('type', 'date');
     todoDueDateInput.placeholder = 'Due date';
     const todoButton = document.createElement('button');
     todoButton.textContent = 'Add';
@@ -66,12 +68,18 @@ function loadDom() {
     tasks.appendChild(tasksContent);
     todo.appendChild(tasks);
     content.appendChild(todo);
+    let task = document.createElement('div');
 
     todoButton.addEventListener('click', () => {
         const index = projects.findIndex(item => item.title === selectedProject.textContent);
         addTodo(projects[index], todoTitleInput.value, todoDueDateInput.value);
+        task = document.createElement('div');
+        task.id = 'task';
+        task.textContent = `Title: ${projects[index].arrayOfTodos.at(-1).title} Due Date: ${projects[index].arrayOfTodos.at(-1).dueDate}`;
+        console.log(task);
         console.log(projects[index].arrayOfTodos);
-        task.textContent = JSON.stringify(projects[index].arrayOfTodos);
+        taskList.appendChild(task);
+        // taskList.textContent = JSON.stringify(projects[index].arrayOfTodos);
 
     });
 }
